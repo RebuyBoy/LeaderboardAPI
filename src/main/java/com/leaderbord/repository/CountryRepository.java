ac5e03e82788ed4b195804284f846618b43e12d2
Sergey M <marchuk.se@gmail.com>
2022-07-10 12:56:17 +0700

    1)db schema: change player -> player_result, move country to player 2)Country service

---
+++ src/main/java/com/leaderbord/repository/CountryRepository.java
@@ -0,0 +1,11 @@
+package com.leaderbord.repository;
+
+import com.leaderbord.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country,Integer> {
+
+    Country getById(String code);
+}
