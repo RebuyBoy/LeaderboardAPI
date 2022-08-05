5633d6f702e809cbada88b2c297921c2cfa0f2b8
Sergey M <marchuk.se@gmail.com>
2022-08-05 23:52:52 +0700

    1) Scheduler  2) AggregateResultDTO

--- src/main/java/com/leaderboard/schedule/GGScheduler.java
+++ src/main/java/com/leaderboard/schedule/GGScheduler.java
@@ -1,6 +1,7 @@
 package com.leaderboard.schedule;
 
 import com.leaderboard.service.GGClientService;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,17 @@ public class GGScheduler {
         this.clientService = clientService;
     }
 
-    public void run(){
+    @Scheduled(cron = "3 0 * * * ?", zone = "UTC-8")
+    public void dailyPromotionData() {
         clientService.getMonthlyData();
+    }
+    @Scheduled(cron = "0 15 1 * ? *", zone = "UTC-8")
+    public void monthlyGroupData() {
         clientService.getDailyData();
     }
+
+//    <minute> <hour> <day-of-month> <month> <day-of-week> <command>
+    //LOGGER parsed
 
 }
 
