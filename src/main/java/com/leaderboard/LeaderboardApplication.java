313c56cb0e20514ac6aa06304e99a157dd5ba166
fichca <w580ii@mail.ru>
2022-09-03 14:15:15 +0300

    Refactoring "LeaderboardApplication"

--- src/main/java/com/leaderboard/LeaderboardApplication.java
+++ src/main/java/com/leaderboard/LeaderboardApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class LeaderboardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LeaderboardApplication.class, args);
     }
-
 }
