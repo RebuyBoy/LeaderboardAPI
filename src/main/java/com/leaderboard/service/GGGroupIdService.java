81d1085b5e29aab9ad13f0c5a5149ca13b81d3ef
Sergey M <marchuk.se@gmail.com>
2022-09-08 06:21:57 +0700

    feat: getByGroupId

--- src/main/java/com/leaderboard/service/GGGroupIdService.java
+++ src/main/java/com/leaderboard/service/GGGroupIdService.java
@@ -5,7 +5,6 @@ import com.leaderboard.repository.GroupIdRepository;
 import com.leaderboard.service.interfaces.GroupIdService;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -18,14 +17,14 @@ public class GGGroupIdService implements GroupIdService {
 
     @Override
     public GroupId saveIfNotExists(GroupId groupId) {
-        Optional<GroupId> optionalGroupId = getByDate(groupId.getDate());
+        Optional<GroupId> optionalGroupId = getByGroupId(groupId);
         return optionalGroupId.isEmpty()
                 ? groupIdRepository.save(groupId)
                 : optionalGroupId.get();
     }
 
     @Override
-    public Optional<GroupId> getByDate(LocalDate date) {
-        return groupIdRepository.getByDate(date);
+    public Optional<GroupId> getByGroupId(GroupId groupId) {
+        return groupIdRepository.getByDateAndPromotionGroupIdAndGameType(groupId.getDate(), groupId.getPromotionGroupId(), groupId.getGameType());
     }
 }
