4f4519dc4b54074f2d4e54144982759c56e0a7c2
Sergey M <marchuk.se@gmail.com>
2022-09-08 06:29:14 +0700

    fix: added @componentScan and fix converter

--- src/main/java/com/leaderboard/configuration/LeaderboardApplication.java
+++ src/main/java/com/leaderboard/configuration/LeaderboardApplication.java
@@ -3,15 +3,19 @@ package com.leaderboard.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = "com.leaderboard")
-@EnableJpaRepositories(basePackages = {"com.leaderboard.repository"})
-@EntityScan(basePackages = "com.leaderboard.entity")
+@SpringBootApplication()
+@EnableJpaRepositories(basePackages = "com.leaderboard.repository")
+@EntityScan(basePackages = {"com.leaderboard.entity", "com.leaderboard.converters"})
+@ComponentScan(basePackages = "com.leaderboard")
 @EnableScheduling
 public class LeaderboardApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(LeaderboardApplication.class, args);
     }
+
 }
