d8efd168ad90f77d035cd0c7cec4ac6d86aabf6f
Sergey M <marchuk.se@gmail.com>
2023-01-12 20:31:46 +0700

    feat: implemented caching for groupResponse

---
+++ src/main/java/com/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
+
+@SpringBootApplication
+@FeignClient
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
